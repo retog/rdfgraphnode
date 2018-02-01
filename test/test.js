f25cc585d7db140c7ba675e65100aaa101504c04
Reto Gmür <reto@factsmission.com>
2018-02-01 08:28:58 +0100

    Added test fetching data

--- test/test.js
+++ test/test.js
@@ -6,7 +6,10 @@ function dc(suffix) {
     return rdf.sym("http://dublincore.org/2012/06/14/dcelements#" + suffix);
 }
 
+
+
 describe('GraphNode', function () {
+    
     describe('#out()', function () {
         it('Getting property value from local graph', function () {
             let dataTurtle = '@prefix dc: <http://dublincore.org/2012/06/14/dcelements#>. \n\
@@ -16,6 +19,38 @@ describe('GraphNode', function () {
             let gn = GraphNode(rdf.sym("http://example.org/"), data);
             let title = gn.out(dc("title")).value;
             assert.equal("An example", title);
+        });
+    });
+
+    describe('Remote data', function () {
+        var express = require('express');
+        var app = express();
+        var server = null;
+
+        before(function(done) {  
+            var path = require('path');
+            //var logger = require('morgan');
+            app.set('port', process.env.PORT || 3123);
+            server = app.listen(app.get('port'),
+            function(){
+                console.log("Express server listening on port " + app.get('port'));
+                //app.use(logger('dev'));
+                app.use(express.static(path.join(__dirname, './served')));
+            });
+            done();
+        });
+        it('Fetching', function (done) {
+            let gn = GraphNode(rdf.sym("http://localhost:"+app.get('port')+"/example.ttl"));
+            gn.fetch().then(gn => 
+                {
+                    let title = gn.out(dc("title")).value;
+                    assert.equal("Another example", title);
+                }).then(done);
+        });
+        after(function(done) {
+            console.log("closing server");;
+            server.close();
+            done();
         });
     });
 });
